e5829418d71a4180626a20125439feac6c38a316
Sk Md Safwaan Uddin <101254419+safwaanDK@users.noreply.github.com>
2023-11-22 19:53:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+const { ApolloServer } = require("apollo-server"); // Import Apollo Server
+const { importSchema } = require("graphql-import"); // Import graphql-import to load schema
+const EtherDataSource = require("./datasource/ethDatasource"); // Import custom data source
+
+const typeDefs = importSchema("./schema.graphql"); // Load schema from file
+
+require("dotenv").config(); // Load environment variables
+
+const resolvers = {
+  Query: {
+    etherBalanceByAddress: (
+      root,
+      _args,
+      { dataSources } // Resolver to get ether balance
+    ) => dataSources.ethDataSource.etherBalanceByAddress(),
+
+    totalSupplyOfEther: (
+      root,
+      _args,
+      { dataSources } // Resolver to get total ether supply
+    ) => dataSources.ethDataSource.totalSupplyOfEther(),
+
+    latestEthereumPrice: (
+      root,
+      _args,
+      { dataSources } // Resolver to get latest eth price
+    ) => dataSources.ethDataSource.getLatestEthereumPrice(),
+
+    blockConfirmationTime: (
+      root,
+      _args,
+      { dataSources } // Resolver to get block confirmation time
+    ) => dataSources.ethDataSource.getBlockConfirmationTime(),
+  },
+};
+
+const server = new ApolloServer({
+  // Create Apollo Server
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    // Data sources
+    ethDataSource: new EtherDataSource(),
+  }),
+});
+
+server.timeout = 0;
+
+server.listen("9000").then(({ url }) => {
+  // Start server on port 9000
+  console.log(`🚀 Server ready at ${url}`);
+});
